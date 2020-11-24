aac9aaed5190fcd36c660fa0ad857cc888b140d8
Ramkumar090986 <rqamkumar8687@gmail.com>
2020-11-24 15:06:38 +0530

    Greens Addrss

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("Greens OMR");
+
+	}
+	private void greensAdayar() {
+		System.out.println("Greens Adayar");
+
+	}
+	private void greensTambaram() {
+		System.out.println("Greens Tambaram");
+
+	}
+	private void greensVelacherry() {
+		System.out.println("Greens Velacherry");
+		
+
+	}
+	private void greensAnnaNagar() {
+		System.out.println("Greens AnnaNagar");
+
+	}
+	public static void main(String[] args) {
+		GreensTech gr=new GreensTech();
+		gr.greensOmr();
+		gr.greensTambaram();
+		gr.greensAdayar();
+		gr.greensAnnaNagar();
+		gr.greensVelacherry();
+	}
+}
